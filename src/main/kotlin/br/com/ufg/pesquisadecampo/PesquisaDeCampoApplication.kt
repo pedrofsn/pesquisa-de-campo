a927ea6f5b12b117667e7e92b757f17752b38ffa
pedrofsn <pedrokra@gmail.com>
2021-03-23 22:46:55 -0300

    project generated in sprint initializer

---
+++ src/main/kotlin/br/com/ufg/pesquisadecampo/PesquisaDeCampoApplication.kt
@@ -0,0 +1,11 @@
+package br.com.ufg.pesquisadecampo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PesquisaDeCampoApplication
+
+fun main(args: Array<String>) {
+	runApplication<PesquisaDeCampoApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/ufg/pesquisadecampo/PesquisaDeCampoApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.ufg.pesquisadecampo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PesquisaDeCampoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
