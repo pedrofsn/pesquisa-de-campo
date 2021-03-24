7cdc0312d586b2e68b6c7ba8dfd60314fe532343
pedrofsn <pedrokra@gmail.com>
2021-03-24 09:21:39 -0300

    Adding JPA, devtools and h2 database

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,9 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-devtools")
+	implementation("com.h2database:h2:1.3.148")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
